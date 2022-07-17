9bdaa65232892a520c5a6710129c252dd4c8e071
perkles <andyrodrigo2@gmail.com>
2022-07-17 13:05:57 -0300

    Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente {
 
 	private String nome;
+	private String sobreNome;
+	private List<Conta> contas;
+	
+	public Cliente(String nome, String sobreNome) {
+		this.nome = nome;
+		this.sobreNome = sobreNome;
+		this.contas = new ArrayList<>();;
+	}
 
 	public String getNome() {
 		return nome;
@@ -9,6 +19,22 @@ public class Cliente {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	public void addConta( Conta conta ) {
+		this.contas.add(conta);
+	}
+	
+	public void verContas() {
+		System.out.println("Contas de " + this.getNome() + " " + this.getSobreNome() + ": " + contas);
 	}
 	
 }
