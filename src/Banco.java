87dab8cb0d3523f75e9176774b267598abc87cf7
perkles <andyrodrigo2@gmail.com>
2022-07-17 11:42:44 -0300

    classes abstraidas

--- src/Banco.java
+++ src/Banco.java
@@ -1,5 +1,8 @@
 
 public class Banco {
+	
+	public String nome;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Banco");
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,20 @@
+
+public class ContaPoupanca {
+	
+	public int agencia;
+	public int numero;
+	public double saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+
+}
